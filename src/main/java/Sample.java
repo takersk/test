58cc5639d748d7025feb78049b11a2a1a2fee253
takersk <jackie0!@>
2018-03-27 13:24:45 +0900

    takersk 3

---
+++ src/main/java/Sample.java
@@ -0,0 +1,9 @@
+public class Sample {
+    public static void main(String[] args) {
+        new Sample().getNum(1);
+    }
+
+    public int getNum(int num) {
+        return num;
+    }
+}
---
+++ src/test/java/JunitTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * https://github.com/junit-team/junit5-samples/tree/r5.1.0/junit5-gradle-consumer
+ *
+ */
+
+@DisplayName("JunitTest")
+//@Disabled
+class JunitTest {
+
+    @Test
+    @DisplayName("test1")
+    void test1() {
+        assertEquals(new Sample().getNum(1), 1);
+        assertEquals(new Sample().getNum(1), 2);
+    }
+}
